c0ee5ff72918115375463c9f92dfe73a74f71dfa
shrinivassab <shrinivassab@gmail.com>
2023-10-01 12:44:39 -0400

    finished animal love project

---
+++ animals/src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import AnimalShow from "./AnimalShow";
+import "./App.css"
+
+function getRandomAnimal() {
+    const animals = ['cow', 'bird', 'gator', 'heart', 'dog', 'cat', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+
+function App() {
+    const [animals, setAnimals] = useState([])
+
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()])
+    }
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index}/>
+    })
+
+    return (
+        <div className="app">
+            <button onClick={handleClick}>Add Animal</button>
+            <div className="animal-list">
+                {renderedAnimals}
+            </div>
+        </div>
+    )
+}
+
+export default App
